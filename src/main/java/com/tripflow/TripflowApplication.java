674fabf0be7bdca1488653b84ac12bd8d6ac0b4f
NehaNaazneen <nehanaazneen17@gmail.com>
2026-08-15 13:32:43 +0530

    module creations

---
+++ src/main/java/com/tripflow/TripflowApplication.java
@@ -0,0 +1,13 @@
+package com.tripflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  TripflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripflow/TripflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
